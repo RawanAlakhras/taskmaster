80d7d279f17174dd23778c7daf7f8b69a71850dc
RawanAlakhras <ralakhras36@gmail.com>
2021-08-29 19:28:44 +0300

    lab26 done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # taskmaster
+
+Task Master is an android application to manage daily tasks built by android studio.
+
+## Home page 
+
+* it have a heading at the top of the page, an image to mock the “my tasks” view, and buttons at the bottom of the page to allow going to the “add tasks” and “all tasks” page.
+
+![hompage](screenshots\homepage.png)
+
+## All Tasks Page
+
+* The all tasks page  just have an image with a back button.
+
+![hompage](screenshots\allTasks.png)
+
+## Add Task page 
+
+* On the “Add a Task” page, allow users to type in details about a new task, specifically a title and a body. When users click the “submit” button, show a “submitted!” label on the page.
+
+![hompage](screenshots\addTask.png)
---
+++ app/src/main/java/com/example/taskmaster/addTask.java
@@ -0,0 +1,29 @@
+package com.example.taskmaster;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class addTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+    }
+
+    public void submitted(View view) {
+
+
+        Toast.makeText(getApplicationContext(),"submitted!",Toast.LENGTH_LONG).show();
+    }
+}
