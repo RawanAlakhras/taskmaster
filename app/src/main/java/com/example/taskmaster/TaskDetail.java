6bb286f81e7f4b5b78873eecccadb520f10e7dd9
RawanAlakhras <ralakhras36@gmail.com>
2021-08-30 16:01:46 +0300

    add lab 27

---
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -0,0 +1,19 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TaskDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+        Intent i =getIntent();
+        TextView tv=findViewById(R.id.title);
+        tv.setText(i.getExtras().getString("title"));
+    }
+}
